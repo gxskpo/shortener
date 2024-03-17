9cff2017ac4011d6bfbe51dce48cea70d103392e
arch-fan <juanserracines@gmail.com>
2024-03-17 15:23:51 +0100

    Swap endpoint to Hono edge

--- app/[key]/route.ts
+++ app/[key]/route.ts
@@ -1,4 +1,27 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
 import { sql } from "@vercel/postgres";
+import type { Urls } from "@/types/urls";
+
+export const runtime = "edge";
+
+const app = new Hono();
+
+app.get("/:key", async (c) => {
+  const { key } = c.req.param();
+
+  const { rows } = await sql<Urls>`SELECT * FROM urls WHERE key = ${key}`;
+
+  if (rows.length > 0) {
+    return c.redirect(rows[0].originalurl);
+  }
+
+  return c.redirect("/404");
+});
+
+export const GET = handle(app);
+
+/* import { sql } from "@vercel/postgres";
 import { redirect } from "next/navigation";
 
 export async function GET(
@@ -22,3 +45,4 @@ export async function GET(
   console.log(redirTo)
   return Response.redirect(redirTo);
 }
+ */
